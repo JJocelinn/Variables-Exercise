510f06364e1766d0390188faa792d6cf3148324c
JJocelinn <jocelinrod3@gmail.com>
2025-03-26 00:24:21 -0700

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String dogName = "Woody";
+            int dogAge = 2;
+            char dogGender = 'm';
+            bool dogIsFriendly = true;
+            double dogWeightInPounds = 14.5;
+            decimal dogHeightInInches = 9.5M;
+
+            Console.WriteLine($"My dog's name is {dogName}. He is {dogAge} years old.\nHe is Male({dogGender}). He is {dogWeightInPounds} pounds and {dogHeightInInches} inches.");
         }
     }
 }
